e762a6ef142c6bb2f42473ce0c2c620528ea73f5
apata <apata@incert.lu>
2021-06-07 11:45:01 +0200

    Exercise 2

---
+++ Reading_images.py
@@ -0,0 +1,36 @@
+from statistics import median
+
+from matplotlib import image
+from matplotlib import pyplot
+
+import numpy as np
+
+image = image.imread('img.jpeg')
+img = np.array(image)
+
+'''
+Exxercise 1
+'''
+
+print(image.shape)
+print(image.dtype)
+
+# Show image
+pyplot.imshow(image)
+pyplot.show()
+
+'''
+Exercise 2
+'''
+
+print(image[2665][3999])
+
+for y in range(0, image.shape[0] - 1):
+    for x in range(0, image.shape[1] - 1):
+        m = median(img[y][x])
+        img[y][x][0] = m
+        img[y][x][1] = m
+        img[y][x][2] = m
+
+pyplot.imshow(img)
+pyplot.show()
